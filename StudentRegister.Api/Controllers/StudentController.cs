1609e5c0226beb3b31d5e519dccbb7475f66d751
dmaramba <dumisani.maramba@gmail.com>
2026-08-26 13:24:15 +0200

    first commit

---
+++ StudentRegister.Api/Controllers/StudentController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudentRegister.Api.Models;
+using StudentRegister.Api.Services;
+
+namespace StudentRegister.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly StudentService _studentService;
+        public StudentController(StudentService studentService)
+        {
+            _studentService = studentService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Student>> GetStudents()
+        {
+            var students = _studentService.GetStudents();
+            return Ok(students);
+        }
+
+        [HttpGet("search")]
+        public ActionResult<List<Student>> SearchStudents(string search)
+        {
+            var students = _studentService.GetStudents();
+            var results = students.Where(x => x.StudentNumber.Contains(search) ||
+                                    x.FirstName.Contains(search) ||
+                                    x.LastName.Contains(search)).ToList();
+            if (results.Any())
+            {
+                return Ok(results);
+            }
+            else
+            {
+                return NotFound($"No records matching {search}");
+            }
+        }
+    }
+}
---
+++ StudentRegister.Api/Services/StudentService.cs
@@ -0,0 +1,46 @@
+﻿using StudentRegister.Api.Models;
+
+namespace StudentRegister.Api.Services
+{
+    public class StudentService
+    {
+        public List<Student> GetStudents()
+        {
+            return new List<Student>
+            {
+                new Student
+                {
+                    Id = 1,
+                    FirstName = "Thabo",
+                    LastName ="Mokoena" ,
+                    StudentNumber = "ST001",
+                    Gender = "M"
+                },
+                new Student
+                {
+                    Id = 2,
+                    FirstName = "Naledi",
+                    LastName ="Dlamini" ,
+                    StudentNumber = "ST002",
+                    Gender = "F"
+                },
+                new Student
+                {
+                    Id = 3,
+                    FirstName = "Sipho",
+                    LastName ="Ndlovu" ,
+                    StudentNumber = "ST003",
+                    Gender = "M"
+                },
+                new Student
+                {
+                    Id = 4,
+                    FirstName = "Tracy",
+                    LastName ="Jones" ,
+                    StudentNumber = "ST004",
+                    Gender = "F"
+                },
+            };
+        }
+    }
+}
